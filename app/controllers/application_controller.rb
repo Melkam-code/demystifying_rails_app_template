a36743a84c33d5b4961e00b73f650bb8ed65ea2e
oiizes <oiizesgitlab@gmail.com>
2019-12-04 19:21:01 +0300

    creating controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
     def hello_world
-        render inline: '<em>"Hello World!"</em>'
+        name = params['name'] || "World"
+        render 'application/hello_world', locals: { name: name }
+    end
+
+    def list_posts
+        connection = SQLite3::Database.new 'db/development.sqlite3'
+        connection.results_as_hash = true
+        posts = connection.execute("SELECT * FROM posts")
+        render 'application/list_posts', locals: { posts: posts }
     end
 end
