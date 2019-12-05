99277a132e3f299f5660bad3bb2846ae41341dd5
oiizes <oiizesgitlab@gmail.com>
2019-12-05 16:24:46 +0300

    refactoring code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,12 @@ class ApplicationController < ActionController::Base
     end
 
     def show_post
-        post = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", params['id']).first
+        post = find_post_by_id(params['id'])
         render 'application/show_post', locals: { post: post }
     end
 
     def new_post
         render 'application/new_post'
-    end
-
     def create_post
         insert_query = <<-SQL
           INSERT INTO posts (title, body, author, created_at)
@@ -34,7 +32,7 @@ class ApplicationController < ActionController::Base
     end
 
     def edit_post
-        post = connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", params['id']).first
+        post = find_post_by_id(params['id'])
         render 'application/edit_post', locals: { post: post }
     end
 
@@ -58,5 +56,9 @@ class ApplicationController < ActionController::Base
         db_connection = SQLite3::Database.new 'db/development.sqlite3'
         db_connection.results_as_hash = true
         db_connection
+    end
+
+    def find_post_by_id(id)
+        connection.execute("SELECT * FROM posts WHERE posts.id = ? LIMIT 1", params['id']).first
     end
 end
