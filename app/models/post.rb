27c59118927568ae2d813530c5e5384b1c96d156
oiizes <oiizesgitlab@gmail.com>
2019-12-06 11:18:22 +0300

    New_record?, save and set_attributes actions on the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,30 @@ class Post
     attr_reader :id, :title, :body, :author, :created_at
 
     def initialize(attributes={})
-       @id = attributes['id']
+       set_attributes(attributes)
+    end
+
+    def set_attributes(attributes)
+       @id = attributes['id'] if new_record?
        @title = attributes['title']
        @body = attributes['body']
        @author = attributes['author']
        @created_at = attributes['created_at']
     end
 
+    def new_record?
+        id.nil?
+    end
+
     def save
+        if new_record?
+            insert
+        else
+            update
+        end
+    end
+
+    def insert
         insert_query = <<-SQL
           INSERT INTO posts (title, body, author, created_at)
           VALUES (?, ?, ?, ?)
@@ -19,6 +35,21 @@ class Post
            body,
            author,
            Date.current.to_s
+    end
+
+    def update
+        update_query = <<-SQL
+          UPDATE posts 
+          SET title = ?, 
+              body  = ?, 
+              author= ?
+          WHERE posts.id = ?
+        SQL
+        connection.execute update_query,
+          title,
+          body,
+          author,
+          id
     end
 
     def self.find(id)
