b23a130f09c58f32312716567bad283a9f8b2cb2
Melkam <melkammekonnen899@gmail.com>
2019-12-11 14:31:44 +0300

    Creating insert and save methods on the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
-class comment 
+class Comment 
     attr_reader :id, :body, :author, :post_id, :created_at
 
     def initialize(attributes={})
@@ -12,4 +12,36 @@ class comment
     def new_record?
         @id.nil?
     end
+
+    def save
+      if new_record?
+        insert
+      else
+        # update # ...not yet defined
+      end
+    end
+  
+    def insert
+      insert_comment_query = <<-SQL
+        INSERT INTO comments (body, author, post_id, created_at)
+        VALUES (?, ?, ?, ?)
+      SQL
+  
+      connection.execute insert_comment_query,
+        @body,
+        @author,
+        @post_id,
+        Date.current.to_s
+    end
+  
+    def self.connection
+      db_connection = SQLite3::Database.new 'db/development.sqlite3'
+      db_connection.results_as_hash = true
+      db_connection
+    end
+  
+    def connection
+      self.class.connection
+    end
+
 end
